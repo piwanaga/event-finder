915a4aff8245b52ab5bd6d6736ed87d3d7cfdbe9
phillip <phillip.iwanaga@gmail.com>
2021-11-12 18:27:34 -0800

    testing

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { authenticateJWT } = require('./middleware/auth')
 // const requestIp = require('request-ip');
+const User = require('./models/User');
 
 const userRoutes = require("./routes/users");
 
