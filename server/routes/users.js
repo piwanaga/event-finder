8822f80854dcbca3138b241d98c978096f0bba49
phillip <phillip.iwanaga@gmail.com>
2021-11-15 10:34:35 -0800

    cors testing

--- server/routes/users.js
+++ server/routes/users.js
@@ -15,11 +15,12 @@ const router = new express.Router();
 router.post("/login", async (req, res, next) => {
     try { 
         const { username, password } = req.body;
-        // const user = await User.authenticate(username, password);
-        // const token = createToken(user);
-        res.set('Access-Control-Allow-Origin', 'https://event-finder.surge.sh')
-        console.log('results from route:', res)
-        // return res.json({ user, token });
+        const user = await User.authenticate(username, password);
+        const token = createToken(user);
+        // res.set('Access-Control-Allow-Origin', 'https://event-finder.surge.sh')
+        // res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+        console.log('response from route:', res)
+        return res.json({ user, token });
     } catch (e) {
       return next(e);
     };
