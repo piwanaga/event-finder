2b88c260cbf63e51fa791fa9997e2ee649c3fac8
phillip <phillip.iwanaga@gmail.com>
2021-11-15 16:03:51 -0800

    cors testing

--- server/app.js
+++ server/app.js
@@ -31,11 +31,11 @@ app.post("/users/login", cors(), async (req, res, next) => {
           'Access-Control-Allow-Methods': 'POST',
           'Access-Control-Allow-Headers': 'Content-Type'
       })
-      const { username, password } = req.body;
-      const user = await User.authenticate(username, password);
-      const token = createToken(user);
-      return res.json({ user, token });
-      // return res.json({ username, password });
+      // const { username, password } = req.body;
+      // const user = await User.authenticate(username, password);
+      // const token = createToken(user);
+      // return res.json({ user, token });
+      return res.json({ username, password });
   } catch (e) {
     return next(e);
   };
